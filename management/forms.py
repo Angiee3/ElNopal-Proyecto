afa7b3625b6021929627e199313f919f8cd6095e
Angiee3 <anpinzon55@misena.edu.co>
2022-09-25 22:14:34 -0500

    Arreglo register

--- management/forms.py
+++ management/forms.py
@@ -71,3 +71,13 @@ class UnitForm(forms.ModelForm):
             'name':forms.TextInput(attrs={'class':'form-control'}),
             'type':forms.Select(attrs={'class':'form-control'}),
         }
+
+class UserRegisterForm(UserCreationForm):
+	email = forms.EmailField()
+	password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
+	password2 = forms.CharField(label='Confirmar contraseña', widget=forms.PasswordInput)
+
+	class Meta:
+		model = User
+		fields = ['username', 'email', 'password1', 'password2']
+		help_texts = {k:"" for k in fields }
