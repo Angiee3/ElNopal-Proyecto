b00a9baeea7d4264c50f21697e871970c2348e8b
Brayan Rivera Gomez <100776460+Brayan883@users.noreply.github.com>
2022-09-29 23:56:21 -0500

    cambio

--- nopal/static/js/filtrar.js
+++ nopal/static/js/filtrar.js
@@ -4,13 +4,6 @@ $(document).ready(function(){
     $(".myDIV .blog-inner").filter(function() {
       console.log($(this));
      let campos_agregar = $(this).text().toLowerCase().indexOf(value) > -1;
-     console.log(campos_agregar);
-      if(campos_agregar === false){
-          document.getElementById('alerta_busqueda').style = 'display:block !important;'
-      }else{
-        document.getElementById('alerta_busqueda').style = 'display:none !important; '
-        console.log(campos_agregar);
-      }
       $(this).toggle(campos_agregar)
     });
   });
