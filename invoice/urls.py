39e21b2b8265de003d6cd0b31054c15e01dd0086
Angiee3 <anpinzon55@misena.edu.co>
2022-09-30 07:48:24 -0500

    Detalles

--- invoice/urls.py
+++ invoice/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path
+
 from .views import *
 from store.views import *
+
 urlpatterns = [
     path('compra/', buy, name="buy"),
+    
     path('compra/modal/<str:modal>/<int:pk>/', buy_actions, name='buy-actions'),
-    # path('compra/inactivas/', buy_status, name="buy-status"),
-    # path('compra/inactivas/<str:modal>/<int:pk>/', buy_status_modal, name="buy-status-modal"),
     path('compra/ver/<int:pk>/', buy_view, name='buy-view'),
     path('compra/eliminar/<int:pk>/', buy_delete, name='buy-delete'),
     path('compra/inactivas/', buy_inactiva, name="buy-inactiva"),
     path('compra/inactivas/<str:modal>/<int:pk>/', buy_inactiva_modal, name="buy-inactiva-modal"),
-
+    
    # path('compra/<str:modal>/<int:pk>', buy_modal, name='buy-modal'),
-
+    
     path('compra/detalle/<int:pk>/', detail_buy, name="buy-detail"),
     path('compra/detalle/<int:pkf>/<str:modal>/<int:pkd>/', detailbuy_modal, name="detailbuy-modal"),
     path('compra/detalle/<int:pk>/cerrar/', detailbuy_cerrar, name="detailbuy-cerrar"),
@@ -22,4 +23,10 @@ urlpatterns = [
     
     path('venta/', sale, name="sale"),
     path('venta/detalle/<int:pk>/', detail_sale, name="sale-detail"),
+    
+    path('registrar/', registerU, name='registerU'),
+    path('registrar<int:pk>/', registerP, name='registerP'),
+    path('usuario/<str:modal>/<int:pk>', user_modal, name="user-modal"),
+    path('registrar/crear/', registerCreatePopup, name = "m_register"),
+
 ]
