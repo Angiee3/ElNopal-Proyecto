2b29123d969bf9b3bab77bfadae2c3f88df012a2
Brayan Rivera Gomez <100776460+Brayan883@users.noreply.github.com>
2022-10-03 20:52:20 -0500

    unidad de medida

--- nopal/settings.py
+++ nopal/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'db_elnopal',
         'USER':'root',
-        'PASSWORD':'JuliethRojas123.',
+        'PASSWORD':'%Brayan2021-2021-2021%#',
         'HOST':'localhost',
         'PORT':'3306'
     }
